0a9d2daa16cd730ab25d80e9efb387db4752fb99
marklepp <markus.leppanen@live.fi>
2020-12-15 23:05:08 +0200

    Add css and drag-resize to Userbar

--- src/public/js/components/Userbar.js
+++ src/public/js/components/Userbar.js
@@ -1,9 +1,12 @@
 const React = require("react");
-const { useState } = React;
+const { useState, useEffect } = React;
 const { MAX_POST_LENGTH } = require("../constants");
+require("../../css/userbar.css");
+const { mouseDrag } = require("../utils");
 
 const Userbar = ({ username }) => {
   const [textvalue, setTextval] = useState("");
+  const [bar, setBar] = useState({ width: "", initialWidth: null });
 
   const handleChange = (event) => {
     setTextval(event.target.value.slice(0, MAX_POST_LENGTH));
@@ -13,18 +16,45 @@ const Userbar = ({ username }) => {
     alert("Submit!");
   };
 
+  const resizeUserBar = mouseDrag(0, ".userbar", (userbar, initialPos) => {
+    const { left, right } = userbar.getBoundingClientRect();
+    const initialWidth = right - left;
+    return (e) => {
+      let newWidth = initialWidth + (e.clientX - initialPos.x);
+      if (newWidth < (bar.initialWidth || initialWidth)) {
+        newWidth = bar.initialWidth || initialWidth;
+      }
+      setBar({
+        width: newWidth,
+        initialWidth: bar.initialWidth || initialWidth,
+      });
+    };
+  });
+
   return (
-    <div>
-      <h1>Microblog</h1>
-      <h2>Hi, {username}</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          What do you think?
-          <textarea value={textvalue} onChange={handleChange}></textarea>
-        </label>
-        <input type="submit" value="Publish ->" />
+    <div className="userbar" style={{ width: bar.width }}>
+      <h1 className="userbar__microblog">Microblog</h1>
+      <h2 className="userbar__hello">Hi, {username}</h2>
+      <form className="userbar__form" onSubmit={handleSubmit}>
+        <textarea
+          className="userbar__thought"
+          name="thought"
+          value={textvalue}
+          onChange={handleChange}
+          placeholder="What do you think?"
+        ></textarea>
+        <input
+          type="submit"
+          value="Publish ->"
+          className="microblog__button userbar__button userbar__button--publish"
+        />
       </form>
-      <button>Find someone</button>
+      <div className="userbar__actions">
+        <button className="microblog__button userbar__button userbar__action">
+          Find someone
+        </button>
+      </div>
+      <div className="userbar__resize--bar" onMouseDown={resizeUserBar}></div>
     </div>
   );
 };
---
+++ src/public/js/utils.js
@@ -0,0 +1,58 @@
+const moveScroll = (element, initialMousePosition) => {
+  let scrollPosition = {
+    // The current scroll
+    left: element.scrollLeft,
+    top: element.scrollTop,
+  };
+  return (event) => {
+    // How far the mouse has been moved
+    const dx = event.clientX - initialMousePosition.x;
+    const dy = event.clientY - initialMousePosition.y;
+
+    // Scroll the element
+    element.scrollTop = scrollPosition.top - dy;
+    element.scrollLeft = scrollPosition.left - dx;
+    element.style.cursor = "grabbing";
+    element.style.userSelect = "none";
+  };
+};
+
+const releaseScroll = (event, element) => {
+  element.style.removeProperty("cursor");
+  element.style.removeProperty("user-select");
+};
+
+const mouseUpHandler = (element, moveHandler, afterRelease) => (event) => {
+  document.removeEventListener("mousemove", moveHandler);
+  afterRelease(event, element);
+};
+
+const mouseDrag = (buttonNumber, selector, onMove, afterRelease = () => {}) => (
+  event
+) => {
+  if (event.button === buttonNumber) {
+    const initialMousePosition = {
+      // Get the current mouse position
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    const element = document.querySelector(selector);
+
+    const moveHandler = onMove(element, initialMousePosition);
+    const upHandler = mouseUpHandler(element, moveHandler, afterRelease);
+
+    document.addEventListener("mousemove", moveHandler);
+    document.addEventListener("mouseup", upHandler, { once: true });
+
+    event.stopPropagation();
+    event.preventDefault();
+    event.cancelBubble = false;
+    return false;
+  }
+};
+
+module.exports = {
+  mouseDrag: mouseDrag,
+  moveScroll: { onMove: moveScroll, afterRelease: releaseScroll },
+};
